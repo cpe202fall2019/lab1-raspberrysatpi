d450d6902b78bfc56ea5068864a94b530fba4c74
raspberrysatpi <raspberrysatpi@gmail.com>
2019-09-28 05:36:04 +0000

    Revision 1

--- lab1.py
+++ lab1.py
@@ -1,4 +1,3 @@
-
 def max_list_iter(int_list):  # must use iteration not recursion
 
 	if int_list is None:
@@ -25,35 +24,46 @@ def reverse_rec(int_list):
  # must use recursion
 
 	if int_list == None:
+
 		raise ValueError("ValueError: int_list == None")
+
 		return
+
 	elif len(int_list) == 0 or len(int_list) == 1:
+
 		return int_list
+
 	else:
 		return [int_list[-1]] + reverse_rec(int_list[:-1])
 	pass
 
-def bin_search(target, low, high, int_list):  # must use recursion
-"""searches for target in int_list[low..high] and returns index if found
-If target is not found returns None. If list is None, raises ValueError """
 
-	if list == None:
+def bin_search(target, low, high, int_list):  # must use recursion
+
+
+	if int_list == None:
 
 		raise ValueError("ValueError: int_list == None")
 
 		return
 
-	elif len(list) == 0:
+	elif len(int_list) == 0 or (len(int_list) == 1 and int_list[0] == target):
 
 		return None
 
 	else:
-		mid = (low + high)/2
+		mid = (low + high) // 2
+
 		if int_list[mid] == target:
+
 			return mid
-		elif int_list[mid] > target
+
+		elif int_list[mid] > target:
+
 			bin_search(target, low, mid, int_list)
-		elif:
+
+
+		elif int_list[mid] < target:
 			bin_search(target, mid, high, int_list)
 
-pass
+	pass
