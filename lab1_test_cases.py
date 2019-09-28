3922babf5fabc6fa901debd58bead0faa5153ff3
raspberrysatpi <raspberrysatpi@gmail.com>
2019-09-28 06:11:12 +0000

    Revision 2: Added code coverage (I think)

--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -20,12 +20,14 @@ class TestLab1(unittest.TestCase):
 
 
     def test_reverse_rec(self):
-
+        # Test None type list
         tlist= None
 
         with self.assertRaises(ValueError):
             reverse_rec(tlist)
 
+        # Test common and edge cases of empty list and list of one element.
+        # Test cases with weird symmetry (5, 5, 5)
         self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
         self.assertEqual(reverse_rec([]), [])
         self.assertEqual(reverse_rec([1]), [1])
@@ -36,15 +38,19 @@ class TestLab1(unittest.TestCase):
         list_val =[0,1,2,3,4,7,8,9,10]
         low = 0
         high = len(list_val)-1
+        # Test None Type list
         tlist = None
 
         with self.assertRaises(ValueError):
             bin_search(0, low, high, tlist)
 
-        self.assertEqual(bin_search(11, 0, len(list_val)-1, list_val), None)
-        self.assertEqual(bin_search(0, 1, len(list_val)-1, list_val), None)
-        self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
+        # Test empty list,searching left, searching right, not finding anything
 
+        self.assertEqual(bin_search(0, 0, len(list_val)-1, list_val), 0)
+        self.assertEqual(bin_search(0, 1, len(list_val)-1, list_val), None)
+        self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4)
+        self.assertEqual(bin_search(8, 0, len(list_val)-1, list_val), 6)
+        self.assertEqual(bin_search(0, 1, 10, []), None)
 if __name__ == "__main__":
         unittest.main()
 
