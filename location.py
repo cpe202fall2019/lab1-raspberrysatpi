7ad01511018edea160c283a237852822c1df023a
raspberrysatpi <raspberrysatpi@gmail.com>
2019-09-28 01:01:19 +0000

    First Revision

--- location.py
+++ location.py
@@ -2,12 +2,21 @@
 
 # represents a location using name, latitude and longitude
 class Location:
-    def __init__(self, name, lat, lon):
-        self.name = name    # string for name of location
-        self.lat = lat      # latitude in degrees (-90 to 90)
-        self.lon = lon      # longitude in degrees (-180 to 180)
+	def __init__(self, name, lat, lon):
+        	self.name = name    # string for name of location
+        	self.lat = lat      # latitude in degrees (-90 to 90)
+        	self.lon = lon      # longitude in degrees (-180 to 180)
 
 # ADD BOILERPLATE HERE (__eq__ and __repr__ functions)
+
+	def __eq__(self, other):
+		if type(self) == type(other):
+			return self.name == other.name and self.lat == other.lat and self.lon == other.lon
+		else:
+			print("Error: Expected two class objects but instead recieved a {0}, and a {1}".format(type(self),type(other)))
+			return
+	def __repr__(self):
+		return "Location('{0}', {1}, {2})".format(self.name, self.lat, self.lon)
 
 def main():
     loc1 = Location("SLO", 35.3, -120.7)
