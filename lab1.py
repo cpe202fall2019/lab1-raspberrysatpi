c1d90a576c041d74bcbb534fdc4979d32c2450c9
raspberrysatpi <raspberrysatpi@gmail.com>
2019-09-28 06:17:54 +0000

    Revision 3: Added Documentation

--- lab1.py
+++ lab1.py
@@ -1,11 +1,17 @@
-def max_list_iter(int_list):  # must use iteration not recursion
+def max_list_iter(int_list):
+
+# Straightforward function that finds the maximum value in a function by itteraton.
+# Continuously updates variable max on each itteration and returns it at the end of the loop.
+# Raises a ValueError if the list is None Type and returns None if the list is empty. 
 
 	if int_list is None:
 
 		raise ValueError('ValueError: int_list == None')
 
 		return	
+
 	elif len(int_list) == 0:
+
 		return None
 
 	else:
@@ -21,7 +27,8 @@ def max_list_iter(int_list):  # must use iteration not recursion
 
 def reverse_rec(int_list):  
 
- # must use recursion
+# Reverses a list int_list by recursively calling reverse_rec. Checks base cases first.
+# Returns a ValueError is the list is None type.
 
 	if int_list == None:
 
@@ -34,12 +41,18 @@ def reverse_rec(int_list):
 		return int_list
 
 	else:
+
 		return [int_list[-1]] + reverse_rec(int_list[:-1])
+
 	pass
 
 
-def bin_search(target, low, high, int_list):  # must use recursion
+def bin_search(target, low, high, int_list):
 
+
+# Recirsive binary search that searches for a target in int_list in a range between low and high.
+# Checks base cases and raises a ValueError if the list is None Type.
+# If the target is not found, returns None, otherwise returns the index of the value.
 
 	if int_list == None:
 
